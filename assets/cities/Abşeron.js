ba108d05cf59d0e88b84a4eabd0a0dadcc22e089
diyarmap <diyarmap@gmail.com>
2025-02-15 16:59:15 +0400

    push bundles

---
+++ assets/cities/Abşeron.js
@@ -0,0 +1,5 @@
+import{v,state,onn,cls,style,attr,none,update}from"../../v.js";import{defaultCityImg}from"../../utils/defaultCityImg.js";export const city={content:t=>v`
+		<div>
+			Abşeron is somver niir to Baku
+		</div>
+	`,img:defaultCityImg("Abşeron.jpg")};
